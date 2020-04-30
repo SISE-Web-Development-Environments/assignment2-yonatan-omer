bd0769cb81daf107fad42067988f9df9add81ebe
oshlomo93 <50537947+oshlomo93@users.noreply.github.com>
2020-04-30 21:02:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function LoginFunction() {
 function WelcomeFunction() {
 	earaseRegText();
 	earaseLogText();
-	window.clearInterval(interval);
+
+	//window.clearInterval(interval);
 	document.getElementById("game").style = "display: none;"
 	document.getElementById("Register").style = "display: none;"
 
